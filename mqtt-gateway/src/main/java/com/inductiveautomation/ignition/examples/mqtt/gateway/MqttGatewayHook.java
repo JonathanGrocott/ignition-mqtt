1e4a5e7ad19f8799a49979f7e21602abcc34b66d
Jonathan Grocott <Jonathan.E.Grocott@gmail.com>
2026-02-03 17:16:49 -0800

    Restore configuration page names

--- mqtt-gateway/src/main/java/com/inductiveautomation/ignition/examples/mqtt/gateway/MqttGatewayHook.java
+++ mqtt-gateway/src/main/java/com/inductiveautomation/ignition/examples/mqtt/gateway/MqttGatewayHook.java
@@ -442,7 +442,7 @@ public class MqttGatewayHook extends AbstractGatewayModuleHook {
                 .addCategory("mqtt-uns-publisher", cat -> cat
                     .label("MQTT UNS Publisher")
                     .position(100)
-                    .addPage("mqtt-uns-config", page -> page
+                    .addPage("Configuration", page -> page
                         .position(10)
                         .mount("/mqtt-uns-publisher", "Configuration", jsModule)
                     )
